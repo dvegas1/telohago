8875b8acd27e222c0ddf51bc8353e2c0d686b03d
darwin <darwinvegas1@gmail.com>
2018-04-17 13:24:43 -0400

    nuevos 1704

--- js/eventos.js
+++ js/eventos.js
@@ -1,19 +1,11 @@
-jQuery(document).ready(function($){
+$(document).ready( function() {
 
-
-
-
-  $('#tituloservicios,#btnweb,#btntiendas,#btnwordpress,#btnjoomla,#btnapi,#btnbd').click(function() {
+$('#tituloservicios,#btnweb,#btntiendas,#btnwordpress,#btnjoomla,#btnapi,#btnbd').click(function() {
 
 
 		// $("#idtabledescripcion").each(function(){
 			 
 			/// document.getElementById("idtabledescripcion").style.display = 'block';
-			 
-			 
-
-
-
 
 if( $('#idtabledescripcion').is(":visible") ){
 	
