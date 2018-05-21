30ac932913c857b64ae9788fbef21fd7ddf17a7c
darwin <darwinvegas1@gmail.com>
2018-05-21 15:48:16 -0400

    2105

---
+++ mail/config.php
@@ -0,0 +1,6 @@
+<?php
+$dbhost='ec2-50-19-224-165.compute-1.amazonaws.com';
+$dbname='dccva6brsskuij';
+$dbuser='qgytbuqtbffbfs';
+$dbpass='0e2daa973151c89bd3fb71a97acdf86711d96996bdcbd1cb1501bf1cb8e26b8b';
+?>
--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,5 +1,6 @@
 <?php
 // Check for empty fields
+
 if(empty($_POST['name'])      ||
    empty($_POST['email'])     ||
    empty($_POST['phone'])     ||
@@ -10,24 +11,32 @@ if(empty($_POST['name'])      ||
    return false;
    }
 
+
 $name = strip_tags(htmlspecialchars($_POST['name']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
+
+$statement = ("INSERT INTO 'CONTACTUS' ('NAME', 'MAIL', 'NUMERO', 'MENSAGE') VALUES ('$name', '$email_address', '$message')");
+
+$statement1= pg_insert($connect,$statement) or die('La consulta fallo: ' . pg_last_error());
+
+
+
+
 // Create the email and send the message
-$to = 'darwinvegas1@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+/*$to = 'darwinvegas1@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "From: darwin.vegas@hotmail.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 //$headers .= "Reply-To: $email_address";
 
-$bool = mail($to,$email_subject,$email_body,$headers);
+$bool = mail($to,$email_subject,$email_body,$headers);*/
+
 if($bool){
     echo "Mensaje enviado";
    return true;
-}else{
-    echo "Mensaje no enviado";
 }
 
 
