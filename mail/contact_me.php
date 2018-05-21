d0368fa1c136f3bcbffe151f4af186f839866e1a
darwin <darwinvegas1@gmail.com>
2018-05-21 16:03:05 -0400

    210503581

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -6,7 +6,7 @@
     die();
   }
 
-  
+
 if(empty($_POST['name'])      ||
    empty($_POST['email'])     ||
    empty($_POST['phone'])     ||
@@ -16,6 +16,15 @@ if(empty($_POST['name'])      ||
    echo "No arguments Provided!";
    return false;
    }
+
+
+$connect = pg_connect("host=ec2-54-163-240-54.compute-1.amazonaws.com port=5432 dbname=d97hqd0uu802gp user=vxoxyrownzuzgx password=0669e05248eea11fe4bdc6d93f82de2248a0dd9a9345ee09215f6f4d2d393862") or die('No se ha podido conectar: ' . pg_last_error());;
+//conectarse a una base de datos llamada "mary" en el host "sheep" con el nombre de usuario y password
+if (!$connect) {
+  echo "Ocurrió un error.\n";
+  exit;
+}
+
 
 
 $name = strip_tags(htmlspecialchars($_POST['name']));
