70def76732c8b6794328d32b9d489d0c71261da8
dvegas1 <33332525+dvegas1@users.noreply.github.com>
2018-05-21 16:04:55 -0400

    Update contact_me.php

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -18,13 +18,17 @@ if(empty($_POST['name'])      ||
    }
 
 
-$connect = pg_connect("host=ec2-54-163-240-54.compute-1.amazonaws.com port=5432 dbname=d97hqd0uu802gp user=vxoxyrownzuzgx password=0669e05248eea11fe4bdc6d93f82de2248a0dd9a9345ee09215f6f4d2d393862") or die('No se ha podido conectar: ' . pg_last_error());;
+$connect = pg_connect("host=ec2-50-19-224-165.compute-1.amazonaws.com port=5432 dbname=dccva6brsskuij user=qgytbuqtbffbfs password=0e2daa973151c89bd3fb71a97acdf86711d96996bdcbd1cb1501bf1cb8e26b8b") or die('No se ha podido conectar: ' . pg_last_error());;
 //conectarse a una base de datos llamada "mary" en el host "sheep" con el nombre de usuario y password
 if (!$connect) {
   echo "Ocurrió un error.\n";
   exit;
 }
 
+$dbhost='ec2-50-19-224-165.compute-1.amazonaws.com';
+$dbname='dccva6brsskuij';
+$dbuser='qgytbuqtbffbfs';
+$dbpass='0e2daa973151c89bd3fb71a97acdf86711d96996bdcbd1cb1501bf1cb8e26b8b';
 
 
 $name = strip_tags(htmlspecialchars($_POST['name']));
